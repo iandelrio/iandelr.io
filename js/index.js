4e9803b5fc9f824712c39c0b33dd2b4ec47a0d4c
iandelrio <iandelriow@gmail.com>
2024-01-11 23:19:43 -0500

    Added keyboard navigation

--- js/index.js
+++ js/index.js
@@ -1,64 +1,73 @@
 
-const sudokuGrid = document.getElementById("sudoku-grid");
-let currentCell = sudokuGrid.querySelector("input");
+document.addEventListener("DOMContentLoaded", function () {
+    const sudokuGrid = document.getElementById("sudoku-grid");
 
-sudokuGrid.addEventListener("keydown", (event) => {
-    const key = event.key;
-    let move = false;
+    sudokuGrid.addEventListener("keydown", (event) => {
 
-    switch (key) {
-        case "ArrowUp":
-            move = moveUp();
-            break;
-        case "ArrowDown":
-            move = moveDown();
-            break;
-        case "ArrowLeft":
-            move = moveLeft();
-            break;
-        case "ArrowRight":
-            move = moveRight();
-            break;
-    }
+        let currentInput = sudokuGrid.querySelector("input");
+        if (document.activeElement.classList.contains("sudoku-cell")) { 
+            currentInput = document.activeElement
+        }
+        const key = event.key;
+        let move = false;
 
-    if (move) {
-        event.preventDefault();
-        currentCell.focus();
-    }
+        switch (key) {
+            case "ArrowUp":
+                move = moveUp();
+                break;
+            case "ArrowDown":
+                move = moveDown();
+                break;
+            case "ArrowLeft":
+                move = moveLeft();
+                break;
+            case "ArrowRight":
+                move = moveRight();
+                break;
+        }
+
+        if (move) {
+            event.preventDefault();
+            currentInput.focus();
+        }
+
+        function moveUp() {
+            const currentRowIndex = currentInput.parentNode.parentNode.rowIndex;
+            if (currentRowIndex > 0) {
+                currentInput = sudokuGrid.rows[currentRowIndex - 1].cells[currentInput.parentNode.cellIndex].querySelector("input");
+                return true;
+            }
+            return false;
+        }
+    
+        function moveDown() {
+            const currentRowIndex = currentInput.parentNode.parentNode.rowIndex;
+            if (currentRowIndex < sudokuGrid.rows.length - 1) {
+                currentInput = sudokuGrid.rows[currentRowIndex + 1].cells[currentInput.parentNode.cellIndex].querySelector("input");
+                return true;
+            }
+            return false;
+        }
+    
+        function moveLeft() {
+            const currentCellIndex = currentInput.parentNode.cellIndex;
+            if (currentCellIndex > 0) {
+                currentInput = currentInput.parentNode.parentNode.cells[currentCellIndex - 1].querySelector("input");
+                return true;
+            }
+            return false;
+        }
+    
+        function moveRight() {
+            const currentCellIndex = currentInput.parentNode.cellIndex;
+            if (currentCellIndex < currentInput.parentNode.parentNode.cells.length - 1) {
+                currentInput = currentInput.parentNode.parentNode.cells[currentCellIndex + 1].querySelector("input");
+                return true;
+            }
+            return false;
+        }
+
+    });
+
+    
 });
-
-function moveUp() {
-    const currentRowIndex = currentCell.parentNode.rowIndex;
-    if (currentRowIndex > 0) {
-        currentCell = sudokuGrid.rows[currentRowIndex - 1].cells[currentCell.cellIndex].querySelector("input");
-        return true;
-    }
-    return false;
-}
-
-function moveDown() {
-    const currentRowIndex = currentCell.parentNode.rowIndex;
-    if (currentRowIndex < sudokuGrid.rows.length - 1) {
-        currentCell = sudokuGrid.rows[currentRowIndex + 1].cells[currentCell.cellIndex].querySelector("input");
-        return true;
-    }
-    return false;
-}
-
-function moveLeft() {
-    const currentCellIndex = currentCell.cellIndex;
-    if (currentCellIndex > 0) {
-        currentCell = currentCell.parentNode.cells[currentCellIndex - 1].querySelector("input");
-        return true;
-    }
-    return false;
-}
-
-function moveRight() {
-    const currentCellIndex = currentCell.cellIndex;
-    if (currentCellIndex < currentCell.parentNode.cells.length - 1) {
-        currentCell = currentCell.parentNode.cells[currentCellIndex + 1].querySelector("input");
-        return true;
-    }
-    return false;
-}
